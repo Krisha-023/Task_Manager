2145f5e38da3b2bce7012d046d9b9c054a182b44
Krisha Mehta <krisha.mehta@unifyapps.com>
2025-06-13 00:11:21 +0530

    feat: add-modal

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,7 +1,26 @@
 import { Header } from "./Header";
 import { useState } from "react";
+import { TaskModal } from "./TaskModal";
 
 export const TaskManager = () => {
-  const [showCreateModal, setShowCreateModal] = useState(false);
-  return <Header onCreateTask={() => setShowCreateModal(true)} />;
+  const [showModal, setShowModal] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = (taskData) => {
+    const newTask = {
+      id: Date.now(),
+      ...taskData,
+      created: new Date().toISOString().split("T")[0],
+    };
+    setTasks([...tasks, newTask]);
+    setShowModal(false);
+  };
+  return (
+    <>
+      <Header onCreateTask={() => setShowModal(true)} />
+      {showModal && (
+        <TaskModal onSave={createTask} onClose={() => setShowModal(false)} />
+      )}
+    </>
+  );
 };
