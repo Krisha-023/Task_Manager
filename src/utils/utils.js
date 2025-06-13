9f0a2ed6b14dba47cbac7e2befbdbda522a84511
Krisha Mehta <krisha.mehta@unifyapps.com>
2025-06-13 13:01:35 +0530

    feat: add-taskcard

---
+++ src/utils/utils.js
@@ -0,0 +1,21 @@
+import { AlertCircle, CheckCircle, Clock, Minus } from "lucide-react";
+import { PRIORITY_OPTIONS } from "../components/constants";
+
+export const getPriorityIcon = (priority) => {
+  const priorityConfig = PRIORITY_OPTIONS.find((p) => p.value === priority);
+  const IconComponent = priorityConfig?.icon || Minus;
+  return <IconComponent className={`w-4 h-4 ${priorityConfig?.color}`} />;
+};
+
+export const getStatusIcon = (status) => {
+  switch (status) {
+    case "todo":
+      return <Clock className="w-4 h-4" />;
+    case "in-progress":
+      return <AlertCircle className="w-4 h-4" />;
+    case "done":
+      return <CheckCircle className="w-4 h-4" />;
+    default:
+      return <Clock className="w-4 h-4" />;
+  }
+};
